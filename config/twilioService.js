b333b15b07d44b18c4e2ad9e0f33f62a9e2a7cc4
akhilmoorkkandathil <akhildasxyz@gmail.com>
2024-01-14 10:34:13 +0530

    some error happened going to previous commit

--- config/twilioService.js
+++
@@ -1,8 +0,0 @@
-const twilio = require('twilio');
-require('dotenv').config();
-
-const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = process.env.TWILIO_AUTH_TOKEN;
-const client = twilio(accountSid, authToken);
-
-module.exports = client;
