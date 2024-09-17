5232ffee3be5bb81a792777f5acf62b039d409aa
akhilmoorkkandathil <akhildasxyz@gmail.com>
2024-09-17 17:26:05 +0530

    EC2 Deployment

--- config/connectDb.js
+++ config/connectDb.js
@@ -4,8 +4,6 @@ require('dotenv').config();
 const connectDb = async ()=>{
     try{
     const mongoUri =`mongodb+srv://akhildasxyz:${process.env.MONGO_PASSWORD}@akhil.bmpur2y.mongodb.net/EcommerceDb?retryWrites=true&w=majority&appName=akhil`;
-         //`mongodb+srv://akhildasxyz:${process.env.MONGO_PASSWORD}@akhil.bmpur2y.mongodb.net/EcommerceDb?retryWrites=true&w=majority`;
-
         await mongoose.connect(mongoUri)
         console.log("Database connected");
 
